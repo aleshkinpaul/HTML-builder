aa475ca89f17cbe615ef5133d248df887060c071
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2021-11-07 21:11:04 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const FOLDER_NAME = 'files';
+const FOLDER_NAME_COPY = 'files-copy';
 
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+const pathFrom = path.join(__dirname, FOLDER_NAME);
+const pathTo = path.join(__dirname, FOLDER_NAME_COPY);
+
+fsPromises.mkdir(pathTo, { recursive: true }, async function (err) {
+  if (err) throw err;
+});
+
+try {
+  readFiles(pathFrom, pathTo);
+}
+catch (err) {
+  throw err;
+}
+
+async function readFiles(dirFrom, dirTo) {
+  const files = await fsPromises.readdir(dirFrom, { withFileTypes: true });
+  files.forEach(async function (file) {
+    if(file.isFile()) {
+      const filePathFrom = path.join(dirFrom, file.name);
+      const filePathTo = path.join(dirTo, file.name);
+
+      fs.copyFile(filePathFrom, filePathTo, (err) => {
+        if (err) throw err;
+      });
+    };
+  });
+}
