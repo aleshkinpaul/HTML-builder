5aff650115900e1a2ed783b5de6a922c577a8ac4
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2021-11-07 20:38:29 +0300

    feat: done 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const FOLDER_NAME = 'secret-folder';
+
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const dirToRead = path.join(__dirname, FOLDER_NAME);
+
+try {
+  readFiles (dirToRead);
+}
+catch (err) {
+  throw err;
+}
+
+async function readFiles(dir) {
+  const files = await fsPromises.readdir(dir, { withFileTypes: true });
+  console.log(`Files in folder "${FOLDER_NAME}":`);
+  files.forEach(async function (file) {
+    if(file.isFile()) {
+      const fileDir = path.join(dirToRead, file.name);
+      const extName = path.extname(fileDir);
+      const baseName = path.basename(fileDir, extName);
+      const stat = await (await fsPromises.open(fileDir)).stat();
+      const size = stat.size / 1000;
+
+      console.log(`${baseName} - ${extName.substring(1)} - ${size}kb`);
+    };
+  });
+}
