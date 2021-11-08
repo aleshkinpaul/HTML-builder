790fac5aff6539f1647a2936b22df960ab109366
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2021-11-08 21:47:18 +0300

    feat: fix copy files in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,18 +7,33 @@ const path = require('path');
 const pathFrom = path.join(__dirname, FOLDER_NAME);
 const pathTo = path.join(__dirname, FOLDER_NAME_COPY);
 
-fsPromises.mkdir(pathTo, { recursive: true }, async function (err) {
-  if (err) throw err;
-});
-
 try {
-  readFiles(pathFrom, pathTo);
+  (async function() {
+    await createFolder(pathTo);
+    await deleteFiles(pathTo);
+    copyFiles(pathFrom, pathTo);
+  })();
 }
 catch (err) {
   throw err;
 }
 
-async function readFiles(dirFrom, dirTo) {
+async function createFolder(folderPath) {
+  fsPromises.mkdir(folderPath, { recursive: true }, async function (err) {
+    if (err) throw err;
+  });
+}
+
+async function deleteFiles(dir) {
+  const files = await fsPromises.readdir(dir, { withFileTypes: true });
+  for await (file of files) {
+    if(file.isFile()) {
+      fs.unlink(path.join(dir, file.name), err => { if (err) throw err });
+    };
+  }
+}
+
+async function copyFiles(dirFrom, dirTo) {
   const files = await fsPromises.readdir(dirFrom, { withFileTypes: true });
   files.forEach(async function (file) {
     if(file.isFile()) {
